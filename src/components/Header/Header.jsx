0cfa37386c62f140234a4e32ad8f59ec112e3bb4
Will Merchan <willmerchan@gmail.com>
2018-03-25 10:29:42 -0400

    add header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Link from "gatsby-link";
+import "./Header.css";
+
+class Header extends Component {
+  render() {
+    return (
+
+      <header className="header">
+        <p>hello</p>
+      </header>
+    );
+  }
+}
+
+export default Header;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Helmet from "react-helmet";
+import Header from "../components/Header/Header";
 import PostListing from "../components/PostListing/PostListing";
 import SEO from "../components/SEO/SEO";
 import config from "../../data/SiteConfig";
