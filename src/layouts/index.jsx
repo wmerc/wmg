a649d1e5bcb07b5d43b8b03f9778067ec54f7475
Will Merchan <willmerchan@gmail.com>
2018-04-10 19:30:31 -0400

    fix meta tag paths

--- src/layouts/index.jsx
+++ src/layouts/index.jsx
@@ -47,13 +47,13 @@ export default class MainLayout extends React.Component {
         <Helmet>
           <title>{`${config.siteTitle} |  ${this.getLocalTitle()}`}</title>
           <meta name="description" content={config.siteDescription} />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-          <link rel="manifest" href="/site.webmanifest">
-          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333b45">
-          <meta name="msapplication-TileColor" content="#333b45">
-          <meta name="theme-color" content="#333b45">
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#333b45" />
+          <meta name="msapplication-TileColor" content="#333b45" />
+          <meta name="theme-color" content="#333b45" />
         </Helmet>
         <Header location={location} />
         {children()}
