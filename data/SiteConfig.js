76d5098006e627fa76cc14b28cded4d8b86b8196
Will Merchan <willmerchan@gmail.com>
2018-04-18 16:28:02 -0400

    fix path url site config

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -3,7 +3,7 @@ module.exports = {
   siteTitle: "Will Merchan", // Site title.
   siteTitleAlt: "Design and Development Portfolio", // Alternative site title for SEO.
   siteLogo: "/logos/logo-1024.png", // Logo used for SEO and manifest.
-  siteUrl: "https://willmerchan.com/", // Domain of your website without pathPrefix.
+  siteUrl: "https://willmerchan.com", // Domain of your website without pathPrefix.
   pathPrefix: "/", // Prefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription: "Projects helped build and things I've learned along the way.", // Website description used for RSS feeds/meta description tag.
   siteRss: "/rss.xml", // Path to the RSS file.
